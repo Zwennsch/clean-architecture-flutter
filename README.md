e45c8af0e12b2687ada86ec39ba10bfa27e7e3e1
Sven Schroeder <schuen@gmx>
2023-11-03 10:05:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# clean_architec
+
+A Flutter project implementing the basics of clean architecture structure
+
+
+
---
+++ android/app/src/main/kotlin/com/example/clean_architec/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clean_architec
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
